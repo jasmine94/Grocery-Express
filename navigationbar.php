1107b924085b6ac5e9e449875a73a1fab06c14bc
jasmine <ang.jasmine94@gmail.com>
2014-09-21 21:15:52 +0800

    to php

---
+++ navigationbar.php
@@ -0,0 +1,31 @@
+<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+	<div class="container">
+	    <div class="navbar-header">
+	        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	        </button>
+	        <a class="navbar-brand" href="index.php">GROCERY EXPRESS</a>
+	    </div>
+	    <div class="navbar-collapse collapse navbar-right">
+	        <ul class="nav navbar-nav">
+	            <li class="active"><a href="index.html"><i class="glyphicon glyphicon-home"></i>HOME</a></li>
+				
+	            <li class="dropdown">
+	              	<a href="#" class="dropdown-toggle" data-toggle="dropdown">CATEGORIES<b class="caret"></b></a>
+				  	<ul class="dropdown-menu">
+	                	<li><a href="food.html">Foods</a></li>
+	                	<li><a href="toiletries.html">Personal Care</a></li>
+	              	</ul>
+	            </li>
+	            <li><a href="cart.html"><i class="glyphicon glyphicon-shopping-cart"></i>CART</a></li>
+	            <li class="dropdown">
+	            <li><a href="co.html">CONTACT US</a></li>
+	            <li class="dropdown">
+	            <li><a href="about.html"><i class="glyphicon glyphicon-info-sign"></i>ABOUT US</a></li>
+	        </ul>
+	    </div><!--/.nav-collapse -->
+	</div>
+</div>
